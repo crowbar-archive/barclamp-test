e5c84a84881a094e483c3b63efbd8cce2caf95de
Greg Althaus <galthaus@austin.rr.com>
2012-08-03 12:05:02 -0500

    Fix barclamp initialization and unix file format issues

--- crowbar_framework/app/controllers/test_controller.rb
+++ crowbar_framework/app/controllers/test_controller.rb
@@ -18,6 +18,7 @@ class TestController < BarclampController
  
   def set_service_object
     @service_object = TestService.new logger
+    @service_object.bc_name = @bc_name
   end
 
   private :set_service_object
